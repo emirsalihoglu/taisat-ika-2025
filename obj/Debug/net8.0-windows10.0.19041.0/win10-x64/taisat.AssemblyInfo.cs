b100289b7206c589dd6b47168caeb2f8a1155160
emirsalihoglu <m.emir_salihoglu@outlook.com>
2025-07-20 11:04:45 +0300

    All components structurally completed without functionality.

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/taisat.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/taisat.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taisat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cacb99823ecd5ace62cff4ee690bc5e2083029d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("taisat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taisat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/leaflet-map.js
@@ -0,0 +1,23 @@
+﻿let map;
+let marker;
+
+function initializeLeafletMap(dotNetHelper) {
+    map = L.map('map').setView([39.92, 32.85], 6);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    map.on('mousemove', function (e) {
+        dotNetHelper.invokeMethodAsync('UpdateMousePosition', e.latlng.lat, e.latlng.lng);
+    });
+
+    map.on('click', function (e) {
+        if (marker) {
+            map.removeLayer(marker);
+        }
+
+        marker = L.marker(e.latlng).addTo(map).bindPopup("Target").openPopup();
+        dotNetHelper.invokeMethodAsync('SetTargetPosition', e.latlng.lat, e.latlng.lng);
+    });
+}
