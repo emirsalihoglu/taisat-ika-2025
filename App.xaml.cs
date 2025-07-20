cacb99823ecd5ace62cff4ee690bc5e2083029d0
emirsalihoglu <m.emir_salihoglu@outlook.com>
2025-07-20 10:40:26 +0300

    inital commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace taisat
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("taisat.MainApplication, taisat, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6415a3a545186e8937.MainApplication.class, crc6415a3a545186e8937.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
